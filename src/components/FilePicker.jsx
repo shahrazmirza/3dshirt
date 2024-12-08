8a15f35c9f774588430e3bab5e5fdef19119fc0e
shahrazmirza <shahrazahmedmirza@live.com>
2024-12-08 19:20:35 +1100

    Commit

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-import CustomButton from './CustomButton'
+import CustomButton from "./CustomButton";
 
 const FilePicker = ({ file, setFile, readFile }) => {
   return (
     <div className="filepicker-container">
       <div className="flex-1 flex flex-col">
-        <input 
+        <input
           id="file-upload"
           type="file"
           accept="image/*"
@@ -17,26 +17,26 @@ const FilePicker = ({ file, setFile, readFile }) => {
         </label>
 
         <p className="mt-2 text-gray-500 text-xs truncate">
-          {file === '' ? "No file selected" : file.name}
+          {file === "" ? "No file selected" : file.name}
         </p>
       </div>
 
       <div className="mt-4 flex flex-wrap gap-3">
-        <CustomButton 
+        <CustomButton
           type="outline"
           title="Logo"
-          handleClick={() => readFile('logo')}
+          handleClick={() => readFile("logo")}
           customStyles="text-xs"
         />
-        <CustomButton 
+        <CustomButton
           type="filled"
-          title="Full"
-          handleClick={() => readFile('full')}
+          title="Pattern"
+          handleClick={() => readFile("full")}
           customStyles="text-xs"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FilePicker
+export default FilePicker;
